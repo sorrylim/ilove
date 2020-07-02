a9496064de4b53b5605443b661d28bcca2af10ab
eodrkd12 <37127443+eodrkd12@users.noreply.github.com>
2020-07-02 15:29:14 +0900

    2020 03 29

--- IOS/IOS/TabView/Story/WriteStoryView.swift
+++ IOS/IOS/TabView/Story/WriteStoryView.swift
@@ -10,7 +10,69 @@ import SwiftUI
 
 struct WriteStoryView : View{
     
+    @State var content=""
+    
+    @State var image : Image?
+    
+    @State var imagePickerVisiable=false
+    @State var inputImage : UIImage?
+    
+    @State var value : CGFloat=0
+    
     var body: some View{
-        Text("WriteStoryView")
+        VStack{
+            MultilineTextField(text:"당신에 대해 이야기 해주세요",content: $content)
+            HStack{
+                Spacer()
+                if image != nil{
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+                Spacer()
+            }
+            .frame(height: 200)
+            
+            Button(action: {
+                self.imagePickerVisiable=true
+            }){
+                Text("사진 추가")
+            }
+                
+            .offset(y: -self.value)
+            .onAppear(){
+                NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { noti in
+                    let value = noti.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+                    let height = value.height-50
+                    
+                    self.value=height
+                }
+                
+                NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main){ noti in
+                    self.value=0
+                }
+            }
+        }
+        .padding()
+        .navigationBarTitle("스토리 작성")
+        .navigationBarItems(trailing: Button(action: {
+            
+        }){
+            Text("등록")
+        })
+        .sheet(isPresented: $imagePickerVisiable, onDismiss: loadImage){
+            ImagePicker(image: self.$inputImage)
+        }
+        .onTapGesture {
+            UIApplication.shared.endEditing()
+        }
+        
+    }
+    
+    func loadImage(){
+        guard let inputImage=inputImage else {
+            return
+        }
+        image=Image(uiImage: inputImage)
     }
 }
